28390948fc09b7925ab227752a4326be5fa04f1e
Cody Cogbill <codeman24@gmail.com>
2023-02-22 18:37:31 -0700

    Introduction with name

---
+++ index.js
@@ -0,0 +1,10 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`)
+};
+
+logTwoValues(2, 3)
+function introductionWithLanguage(name, language){
+console.log(`Hi, my name is ${name} and I am learning to program in ${language}`) 
+}
+
+introductionWithLanguage("Cody", "JavaScript")
